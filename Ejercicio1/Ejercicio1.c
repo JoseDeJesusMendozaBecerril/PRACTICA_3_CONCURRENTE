8c10a2df22bcc0b74d73a882b971dd31aa93207e
tealflipper <arturofv009@gmail.com>
2020-06-15 22:53:03 -0500

    quite prints

--- Ejercicio1/Ejercicio1.c
+++ Ejercicio1/Ejercicio1.c
@@ -11,7 +11,7 @@
 #define MAESTRO 0
 
 // Tamaño del arreglo
-const int ARRAY_TAM = 100;
+const int ARRAY_TAM = 1000;
 
 int main(int argc, char *argv[])
 {
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
 
       // Llenarlo con los número 1, 2,...,ARRAY_TAM
       for (int i = 0; i < ARRAY_TAM; i++) arreglo[i] = i + 1;
-      for (int i = 0; i < ARRAY_TAM; i++) printf("%d ",arreglo[i]);
+      //for (int i = 0; i < ARRAY_TAM; i++) printf("%d ",arreglo[i]);
 
       // Para cada proceso esclavo (q=1,...,comm_size) enviarle solamente los
       // elementos de la parte del arreglo que le corresponde.
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
       for(int q = 1; q <= comm_size - 1; q++){ /* 1 2 3 4 ... numprocesos */
           if(q <= (ARRAY_TAM % (comm_size))) tamSubarreglo++;
           if (q == 1) inicio = tamSubarreglo;
-          printf("\nLe mando a process PID %d desde %d va a sumar un total de %d elementos ",q,inicio,tamSubarreglo);
+          //printf("\nLe mando a process PID %d desde %d va a sumar un total de %d elementos ",q,inicio,tamSubarreglo);
           MPI_Send(&arreglo[inicio], tamSubarreglo, MPI_INT, q, 0, MPI_COMM_WORLD); //pos inicio arr , numElementos , tipo dato , proceso dest , 0 , comunicador
           inicio = inicio + tamSubarreglo; //** check
           tamSubarreglo = ARRAY_TAM / (comm_size);
@@ -58,23 +58,23 @@ int main(int argc, char *argv[])
       inicio=0;
       if(my_rank < (ARRAY_TAM % (comm_size))){ tamSubarreglo++;}
       else{tamSubarreglo = ARRAY_TAM / (comm_size);}
-      int sumaM=0;
+      long sumaM=0;
       for(int i=0; i <tamSubarreglo; i++ ){
          sumaM+=arreglo[i];
       }
-      printf("\nSoy process PID %d sumare desde %d va a sumar un total de %d elementos ",my_rank,inicio,tamSubarreglo);
-      printf("\nSoy proceso con PID %d sume total %d\n",my_rank,sumaM);
+      //printf("\nSoy process PID %d sumare desde %d va a sumar un total de %d elementos ",my_rank,inicio,tamSubarreglo);
+      //printf("\nSoy proceso con PID %d sume total %d\n",my_rank,sumaM);
 
       // Recibir el resultado parcial de cada esclavo (q=1,...,comm_size)
       // y acumularlo para dar el resultado final.
-      int parcial=0;
-      int final=0;
+      long parcial=0;
+      long final=0;
       for(int q = 1; q <= comm_size - 1; q++){ /* 1 2 3 4 ... numprocesos */
           MPI_Recv(&parcial, 1, MPI_INT, q, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
           final = final + parcial;
       }
 
-      printf("\nResultado final %d\n",final + sumaM);
+      printf("\nResultado final %ld\n",final + sumaM);
 
       free(arreglo);
    }
@@ -96,7 +96,7 @@ int main(int argc, char *argv[])
       for(int i=0; i <tamSubarreglo; i++ ){
          suma+=arreglo[i];
       }
-      printf("Soy proceso con PID %d sume total %d\n",my_rank,suma);
+      //printf("Soy proceso con PID %d sume total %d\n",my_rank,suma);
 
       for(int i=0; i < comm_size - 1 ; i++ ){
           MPI_Send(&suma, 1, MPI_INT, MAESTRO, 0, MPI_COMM_WORLD);
